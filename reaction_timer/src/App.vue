3d64534399241c7aa4c3742881904c8307934de4
ZingerEngineer <ramyisez@gmail.com>
2022-09-28 17:09:22 +0200

    reaction_timer *WIP

---
+++ reaction_timer/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+</template>
+
+<script>
+import Block from "./components/Block.vue"
+import Results from "./components/Results.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Block,Results
+  }
+}
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+}
+</style>
